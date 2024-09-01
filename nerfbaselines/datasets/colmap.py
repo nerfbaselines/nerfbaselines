34740fe165519a6f52ce5a2f4689d7bfbad453e6
Jonas Kulhanek <jonas.kulhanek@live.com>
2024-09-01 18:58:09 +0200

    Make COLMAP dataset sampling_masks compatible with colmap cli

--- nerfbaselines/datasets/colmap.py
+++ nerfbaselines/datasets/colmap.py
@@ -302,7 +302,7 @@ def load_colmap_dataset(path: Union[Path, str],
         image_names.append(image.name)
         image_paths.append(str(images_path / image.name))
         if sampling_mask_paths is not None:
-            sampling_mask_paths.append(str(sampling_masks_path / Path(image.name).with_suffix(".png")))
+            sampling_mask_paths.append(str(sampling_masks_path / Path(image.name + ".png")))
 
         # rotation = qvec2rotmat(image.qvec).astype(np.float32)
         # translation = image.tvec.reshape(3, 1).astype(np.float32)
