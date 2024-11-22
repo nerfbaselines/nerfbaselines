3ee925d725a85739c6bb5ffa4d0a1e01571aacd2
Jonas Kulhanek <jonas.kulhanek@live.com>
2024-11-22 19:56:08 +0100

    Move huggingface jkulhanek->nerfbaselines

--- nerfbaselines/_constants.py
+++ nerfbaselines/_constants.py
@@ -4,7 +4,7 @@ DOCKER_REPOSITORY = "ghcr.io/nerfbaselines/nerfbaselines"
 NB_PREFIX = os.path.expanduser(os.environ.get("NERFBASELINES_PREFIX", "~/.cache/nerfbaselines"))
 
 CODE_REPOSITORY = "github.com/nerfbaselines/nerfbaselines"
-RESULTS_REPOSITORY = "huggingface.co/jkulhanek/nerfbaselines"
-DATASETS_REPOSITORY = "huggingface.co/datasets/jkulhanek/nerfbaselines-data"
-SUPPLEMENTARY_RESULTS_REPOSITORY = "huggingface.co/datasets/jkulhanek/nerfbaselines-supplementary"
+RESULTS_REPOSITORY = "huggingface.co/nerfbaselines/nerfbaselines"
+DATASETS_REPOSITORY = "huggingface.co/datasets/nerfbaselines/nerfbaselines-data"
+SUPPLEMENTARY_RESULTS_REPOSITORY = "huggingface.co/datasets/nerfbaselines/nerfbaselines-supplementary"
 del os
