721d82f7f38461dfa36033fc2f1dc6e62908e347
Jonas Kulhanek <jonas.kulhanek@live.com>
2024-11-25 14:28:04 +0100

    Skip windows actions due to firewall issues

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import pytest
 
@@ -16,12 +17,14 @@ def _call_cli(args):
         sys.argv = old_argv
 
 
+# TODO: Make tests work on windows in GH actions (currently firewall issue in CI)
+@pytest.mark.skipif(sys.platform == "win32" and os.getenv("CI") == "true", reason="CI firewall windows issue")
 def test_export_results_table_tutorial_markdown():
     _call_cli("generate-dataset-results --output-type markdown --dataset mipnerf360".split())
     _call_cli("generate-dataset-results --output-type markdown --dataset mipnerf360 --scenes garden,bonsai".split())
     _call_cli("generate-dataset-results --output-type markdown --dataset mipnerf360 --method-links website".split())
         
-
+@pytest.mark.skipif(sys.platform == "win32" and os.getenv("CI") == "true", reason="CI firewall windows issue")
 def test_export_results_table_tutorial_latex():
     _call_cli("generate-dataset-results --output-type latex --dataset mipnerf360".split())
     _call_cli("generate-dataset-results --output-type latex --dataset mipnerf360 --scenes garden,bonsai".split())
