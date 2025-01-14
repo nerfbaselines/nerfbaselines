0cb32e684dcc63af0a194646100cb1f9559478b7
Jonas Kulhanek <jonas.kulhanek@live.com>
2025-01-14 20:09:08 +0100

    test: Add plyfile viewer test

--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -1,6 +1,9 @@
+import io
+import numpy as np
 import pytest
 import requests
 import platform
+import plyfile
 
 
 def test_viewer_simple_http_server():
@@ -10,7 +13,12 @@ def test_viewer_simple_http_server():
 
     from nerfbaselines.viewer import Viewer
 
-    with Viewer() as viewer:
+    dataset = {
+        "points3D_xyz" : np.random.rand(100, 3),
+        "points3D_rgb" : np.random.rand(100, 3).astype(np.uint8),
+    }
+
+    with Viewer(train_dataset=dataset) as viewer:
         assert viewer.port is not None
 
         # Test index.html
@@ -18,6 +26,13 @@ def test_viewer_simple_http_server():
         response = requests.get(host)
         assert response.status_code == 200
         assert response.text.startswith("<!DOCTYPE html>")
+
+        # Test get dataset pointcloud
+        response = requests.get(f"{host}/dataset/pointcloud.ply")
+        assert response.status_code == 200
+        with io.BytesIO(response.content) as f:
+            plydata = plyfile.PlyData.read(f)
+        assert plydata["vertex"].count == len(dataset["points3D_xyz"])
 
         with Viewer(port=viewer.port) as viewer2:
             assert viewer2.port == viewer.port+1
