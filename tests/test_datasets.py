1445263588b1097fec0406d6d610beeda2f535f3
Jonáš Kulhánek <jonas.kulhanek@live.com>
2025-04-29 14:44:44 +0200

    Add zipnerf dataset (#61)
    * Add zipnerf dataset
    
    * Update zipnerf dataset tests

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -88,6 +88,17 @@ def test_mipnerf360_dataset(tmp_path):
     assert test_dataset["images"][0].shape == (828, 1256, 3)
 
 
+@pytest.mark.dataset("zipnerf")
+def test_zipnerf_dataset(tmp_path):
+    train_dataset, test_dataset = _test_generic_dataset(
+        tmp_path, "zipnerf", "alameda", train_size=1517, test_size=217)
+    assert train_dataset["metadata"].get("evaluation_protocol") == "nerf"
+    assert test_dataset["metadata"].get("evaluation_protocol") == "nerf"
+    test_images = [os.path.relpath(x, test_dataset["image_paths_root"]) for x in test_dataset["image_paths"][:5]]
+    assert test_images == ['indoor_DSC06836.JPG', 'indoor_DSC07249.JPG', 'indoor_DSC08034.JPG', 'indoor_DSC08058.JPG', 'indoor_DSC06500.JPG']
+    assert test_dataset["images"][0].shape == (793, 1394, 3)
+
+
 @pytest.mark.dataset("llff")
 def test_llff_dataset(tmp_path):
     train_dataset, test_dataset = _test_generic_dataset(
