d9ab7beec61de4a6da2c88af170eddb3183e5731
Jonas Kulhanek <jonas.kulhanek@live.com>
2025-05-03 12:36:38 +0200

    Add H3DGS dataset description

--- nerfbaselines/datasets/hierarchical_3dgs_spec.py
+++ nerfbaselines/datasets/hierarchical_3dgs_spec.py
@@ -8,7 +8,7 @@ register({
     "metadata": {
         "id": "hierarchical-3dgs",
         "name": "Hierarchical 3DGS",
-        "description": "",
+        "description": "Hierarchical 3DGS is a dataset released with H3DGS paper. We implement the two public single-chunks scenes (SmallCity, Campus) used for evaluation. To collect the dataset, authors used a bicycle helmet on which they mounted 6 GoPro HERO6 Black cameras (5 for the Campus scene). They collected SmallCity and BigCity captures on a bicycle, riding at around 6–7km/h, while Campus was captured on foot wearing the helmet. Poses were estimated using COLMAP with custom parameters and hierarchical mapper. Additinal per-chunk bundle adjustment was performed. It is recommended to use exposure modeling with this dataset",
         "paper_title": "A Hierarchical 3D Gaussian Representation for Real-Time Rendering of Very Large Datasets",
         "paper_authors": ["Bernhard Kerbl", "Andreas Meuleman", "Georgios Kopanas", "Michael Wimmer", "Alexandre Lanvin", "George Drettakis"],
         "paper_link": "https://repo-sam.inria.fr/fungraph/hierarchical-3d-gaussians/hierarchical-3d-gaussians_low.pdf",
