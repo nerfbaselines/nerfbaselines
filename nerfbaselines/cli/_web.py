29cde478ea58bf34e3bd44328372487c321a2c5c
Jonas Kulhanek <jonas.kulhanek@live.com>
2025-05-14 08:27:26 +0200

    Add port option for web dev

--- nerfbaselines/cli/_web.py
+++ nerfbaselines/cli/_web.py
@@ -12,18 +12,19 @@ web_click_group = click.Group("web")
 ))
 @click.option("--data", "data_path", default=None, help="Path to data directory. If not provided, data is generated from the NerfBaselines repository.")
 @click.option("--datasets", default=None, help="List of comma separated dataset ids to include.")
+@click.option("--port", type=int, default=5500, help="HTTP port for the server.")
 @click.option("--docs", "include_docs",
               type=click.Choice(['all', 'latest', 'none']), 
               default="none", 
               show_default=True,
               help="Whether to include the documentation page for all versions, the latest, or none.")
-def _(data_path, datasets, include_docs=None):
+def _(data_path, datasets, include_docs=None, port=5500):
     if include_docs == "none":
         include_docs = None
     datasets = datasets.split(",") if datasets else None
     if include_docs == "none":
         include_docs = None
-    start_dev_server(data=data_path, datasets=datasets, include_docs=include_docs)
+    start_dev_server(data=data_path, datasets=datasets, include_docs=include_docs, port=port)
 
 
 @web_click_group.command("build", cls=NerfBaselinesCliCommand, help=(
