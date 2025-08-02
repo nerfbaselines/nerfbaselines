f6d88dd916061be509ec717606506b8eb442c60f
Jonas Kulhanek <jonas.kulhanek@live.com>
2025-08-02 09:30:59 +0200

    Fix WildGaussians after upgrade to 1.2.10

--- nerfbaselines/methods/wild_gaussians_spec.py
+++ nerfbaselines/methods/wild_gaussians_spec.py
@@ -14,7 +14,7 @@ WildGaussiansMethodSpec: MethodSpec = {
         "install_script": r"""
 git clone https://github.com/jkulhanek/wild-gaussians.git
 cd wild-gaussians
-git checkout 481e46b478fc259e991bf4873b139c0ad33613ec
+git checkout 8e58093a32dd7f9dbba19e482b24904c168fd379
 if [ "$NERFBASELINES_DOCKER_BUILD" != "1" ]; then
 conda install -y gcc_linux-64=11 gxx_linux-64=11 make=4.3 cmake=3.28.3 -c conda-forge
 fi
